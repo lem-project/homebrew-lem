23cdefdf2d882bb7f297be4fdb156036e0da1ffa
cxxxr <g23tlm@gmail.com>
2023-05-20 20:07:33 +0900

    update

--- Formula/lem.rb
+++ Formula/lem.rb
@@ -1,28 +1,12 @@
 class Lem < Formula
-  desc "Common Lisp editor/IDE with high expansibility"
+  desc " Common Lisp editor/IDE with high expansibility "
   homepage "https://github.com/lem-project/lem"
-  url "https://github.com/lem-project/lem/releases/download/v2.0.0-alpha/lem.zip"
-  sha256 "113aa03bb894459b17b3e9e087413f5ed414f87831be1f78fb7ec854d07732c1"
+  url "https://github.com/lem-project/lem/releases/download/v2.0.0-alpha/lem-darwin-arm64-ncurses.zip"
+  sha256 "fe2485e36ecf0d3668a113d4212ecb17f96230f54f00fb7beec927302b53d30a"
   license "MIT"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    bin.install "lem"
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test lem`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    prefix.install Dir["*"]
+    bin.install_symlink prefix/"lem"
   end
 end
